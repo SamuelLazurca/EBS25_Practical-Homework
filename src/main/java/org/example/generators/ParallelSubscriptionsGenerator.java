a75a982664a9e9bdbd5ba0f81af486c5048cb62d
Stefcool8 <stefan.nastasiu8@gmail.com>
2025-04-10 15:10:30 +0300

    stef-dev: Some import improvements.

--- src/main/java/org/example/generators/ParallelSubscriptionsGenerator.java
+++ src/main/java/org/example/generators/ParallelSubscriptionsGenerator.java
@@ -3,8 +3,14 @@ package org.example.generators;
 import org.example.schema.Schema;
 import org.example.schema.SchemaField;
 
-import java.util.*;
-import java.util.concurrent.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class ParallelSubscriptionsGenerator {
     public static void generateSubscriptionsMultiThreaded (
